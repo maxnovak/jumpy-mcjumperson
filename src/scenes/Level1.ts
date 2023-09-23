09f619385deb2a06e832b38ff72391a005a5c528
Maxwell Novak <novak.max@gmail.com>
2023-09-23 13:25:30 -0400

    Rename layers to be more helpful

--- src/scenes/Level1.ts
+++ src/scenes/Level1.ts
@@ -2,19 +2,19 @@ import { Container, Sprite, TilingSprite } from "pixi.js";
 import { IScene } from "./types";
 import { Manager } from "../Manager";
 
-const layer1Velocity = 0.2;
-const layer2Velocity = 0.35;
-const layer3Velocity = 0.60;
-const layer4Velocity = 0.85;
-const layer5Velocity = 1.5;
+const background2Velocity = 0.2;
+const background3Velocity = 0.35;
+const background4Velocity = 0.60;
+const groundVelocity = 0.85;
+const foregroundVelocity = 1.5;
 
 export class Level1 extends Container implements IScene {
     private background: Sprite;
-    private layer1: TilingSprite;
-    private layer2: TilingSprite;
-    private layer3: TilingSprite;
-    private layer4: TilingSprite;
-    private layer5: TilingSprite;
+    private background2: TilingSprite;
+    private background3: TilingSprite;
+    private background4: TilingSprite;
+    private ground: TilingSprite;
+    private foreground: TilingSprite;
     private layer1Velocity = 0;
     private layer2Velocity = 0;
     private layer3Velocity = 0;
@@ -29,24 +29,24 @@ export class Level1 extends Container implements IScene {
         this.background.position.y = Manager.height / 2;
         this.background.anchor.set(0.5);
         this.background.scale.set(2, 2);
-        this.layer1 = TilingSprite.from("background_2", {width: Manager.width, height: Manager.height});
-        this.layer1.scale.set(2, 2);
-        this.layer2 = TilingSprite.from("background_3", {width: Manager.width, height: Manager.height});
-        this.layer2.scale.set(2, 2);
-        this.layer3 = TilingSprite.from("background_4", {width: Manager.width, height: Manager.height});
-        this.layer3.scale.set(2, 2);
-        this.layer4 = TilingSprite.from("background_5", {width: Manager.width, height: Manager.height});
-        this.layer4.scale.set(2, 2);
-        this.layer5 = TilingSprite.from("background_6", {width: Manager.width, height: Manager.height});
-        this.layer5.scale.set(2, 2);
+        this.background2 = TilingSprite.from("background_2", {width: Manager.width, height: Manager.height});
+        this.background2.scale.set(2, 2);
+        this.background3 = TilingSprite.from("background_3", {width: Manager.width, height: Manager.height});
+        this.background3.scale.set(2, 2);
+        this.background4 = TilingSprite.from("background_4", {width: Manager.width, height: Manager.height});
+        this.background4.scale.set(2, 2);
+        this.ground = TilingSprite.from("background_5", {width: Manager.width, height: Manager.height});
+        this.ground.scale.set(2, 2);
+        this.foreground = TilingSprite.from("background_6", {width: Manager.width, height: Manager.height});
+        this.foreground.scale.set(2, 2);
 
 
         this.addChild(this.background);
-        this.addChild(this.layer1);
-        this.addChild(this.layer2);
-        this.addChild(this.layer3);
-        this.addChild(this.layer4);
-        this.addChild(this.layer5);
+        this.addChild(this.background2);
+        this.addChild(this.background3);
+        this.addChild(this.background4);
+        this.addChild(this.ground);
+        this.addChild(this.foreground);
 
         document.addEventListener("keydown", this.onKeyDown.bind(this));
         document.addEventListener("keyup", this.onKeyUp.bind(this));
@@ -54,18 +54,18 @@ export class Level1 extends Container implements IScene {
 
     private onKeyDown(e: KeyboardEvent): void {
         if (e.key === "d" || e.key === "ArrowRight") {
-            this.layer1Velocity = -layer1Velocity;
-            this.layer2Velocity = -layer2Velocity;
-            this.layer3Velocity = -layer3Velocity;
-            this.layer4Velocity = -layer4Velocity;
-            this.layer5Velocity = -layer5Velocity;
+            this.layer1Velocity = -background2Velocity;
+            this.layer2Velocity = -background3Velocity;
+            this.layer3Velocity = -background4Velocity;
+            this.layer4Velocity = -groundVelocity;
+            this.layer5Velocity = -foregroundVelocity;
         }
         if (e.key === "a" || e.key === "ArrowLeft") {
-            this.layer1Velocity = layer1Velocity;
-            this.layer2Velocity = layer2Velocity;
-            this.layer3Velocity = layer3Velocity;
-            this.layer4Velocity = layer4Velocity;
-            this.layer5Velocity = layer5Velocity;
+            this.layer1Velocity = background2Velocity;
+            this.layer2Velocity = background3Velocity;
+            this.layer3Velocity = background4Velocity;
+            this.layer4Velocity = groundVelocity;
+            this.layer5Velocity = foregroundVelocity;
         }
     }
 
@@ -80,10 +80,10 @@ export class Level1 extends Container implements IScene {
     }
 
     public update(framesPassed: number): void {
-        this.layer1.tilePosition.x += this.layer1Velocity * framesPassed;
-        this.layer2.tilePosition.x += this.layer2Velocity * framesPassed;
-        this.layer3.tilePosition.x += this.layer3Velocity * framesPassed;
-        this.layer4.tilePosition.x += this.layer4Velocity * framesPassed;
-        this.layer5.tilePosition.x += this.layer5Velocity * framesPassed;
+        this.background2.tilePosition.x += this.layer1Velocity * framesPassed;
+        this.background3.tilePosition.x += this.layer2Velocity * framesPassed;
+        this.background4.tilePosition.x += this.layer3Velocity * framesPassed;
+        this.ground.tilePosition.x += this.layer4Velocity * framesPassed;
+        this.foreground.tilePosition.x += this.layer5Velocity * framesPassed;
     }
 }
